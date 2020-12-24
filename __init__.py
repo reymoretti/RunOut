79fbe6c2b61f6046c20e7ce47dab011198df516a
reymoretti <76179319+reymoretti@users.noreply.github.com>
2020-12-24 19:33:52 +0100

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'jvsdnoewnvi243fcn'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+from app import routes
+
+from models import Customer, Foodseller
+from app.models import Offer
+db.create_all()
+db.session.commit()
